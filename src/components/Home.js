ccc64d1796f1558f4891ae33041f767f42628ae3
xYangWolfx <miguelferreira1996@gmail.com>
2023-11-28 19:00:23 +0000

    changes to background and home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,43 @@ import logo from "./images/background.jpeg";
 
 function Home({ loggedInUser, handleLogout }) {
   return (
-    <div className="page-background">
+    <div>
+      <div className="event-info">
+        <h2>ENDA Coimbra - dias 2 e 3 de dezembro</h2>
+        <p>
+          O Encontro Nacional de Direções Associativas terá início pelas 10h00
+          de dia 2 de dezembro, no Auditório António Arnaut na Escola Superior
+          de Tecnologia da Saúde de Coimbra, sito em São Martinho do Bispo -
+          Coimbra.
+        </p>
+        <p>
+          A Comissão Organizadora apresenta a seguinte proposta de ordem de
+          trabalhos:
+        </p>
+        <ul>
+          <li>Plenário A: Plenário Inicial;</li>
+          <li>Plenário B: Ação Social e Abandono Escolar;</li>
+          <li>Plenário C: Empregabilidade;</li>
+          <li>Plenário D: Internacionalização;</li>
+          <li>
+            Plenário E: Regime Jurídico do Associativismo Jovem;
+          </li>
+          <li>Plenário F: Medidas Conjuntas;</li>
+          <li>Plenário G: Plenário Final.</li>
+        </ul>
+      </div>
+
+      <div className="credentials-info">
+        <h2>Receção aos delegados e participantes:</h2>
+        <p>
+          O levantamento de credenciais estará aberto nos seguintes horários:
+        </p>
+        <ul>
+          <li>Dia 1 - 19h00 até 23h00</li>
+          <li>Dia 2 - 09h00 até 21h00</li>
+          <li>Dia 3 - 09h00 até 21h00</li>
+        </ul>
+      </div>
     </div>
   );
 }
